febd37b1682e6b9f442260d5d1aa38a240616c2b
szgeri28 <szabogeri8@gmail.com>
2023-09-24 16:19:23 +0200

    Hibák kommentelése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //[N_ELEMENTS]
+    std::cout << '1-100 ertekek duplazasa' //Hianyzik a ; végérõl és "" közé kellett volna írni
+    for (int i = 0;) //Hiányos a for ciklus
     {
-        b[i] = i * 2;
+        b[i] = i * 2; // b[i] helyett b[i-1] kéne írni
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //0-tol 0-ig megy? ez igy nem jo
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Hianyzo értek és nincs endl hogy jól nézzen ki
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // , helyett ; kell írni
     {
-        atlag += b[i]
+        atlag += b[i] // Itt is hiányzik ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
